7a3f01b67fa493c74c43c6b5a3b6dcec7a7beabb
joao <joao17258@gmail.com>
2026-08-03 14:28:34 -0300

    feat(physics): result and conservation check types

--- src/oec/physics/__init__.py
+++ src/oec/physics/__init__.py
@@ -2,16 +2,19 @@
 
 from oec.physics.errors import ConservationError, PhysicsError, PhysicsEvaluationError
 from oec.physics.laws import BoundaryCondition, ConservationLaw, MaterialProperty, PhysicalLaw
+from oec.physics.result import ConservationCheck, PhysicsResult
 from oec.physics.types import PhysicsDomain, Residual, ValidityFrame
 
 __all__ = [
     "BoundaryCondition",
     "ConservationError",
+    "ConservationCheck",
     "ConservationLaw",
     "MaterialProperty",
     "PhysicsDomain",
     "PhysicsError",
     "PhysicsEvaluationError",
+    "PhysicsResult",
     "PhysicalLaw",
     "Residual",
     "ValidityFrame",
